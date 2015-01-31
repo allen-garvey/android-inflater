718e147250be3a4e8cfa2f1b7d91676c9db2abaf
Allen Garvey <allengarvey@hotmail.com>
2015-01-30 22:52:33 -0500

    update app to display error message on parse xml failure and for xml and variable declarion parsing to use the same methods

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,10 @@
 			camelCase : false,
 			visibility : $scope.xml_opt_visibility,
 			makeFinal : false,
-			addButtonOnClickListener : false
+			addButtonOnClickListener : true
+		};
+		$scope.var_declaration_options = {
+			addButtonOnClickListener : true
 		};
 		$scope.isSelectEnabled = function(){
 			var id = $scope.currentTab === 0 ? 'xml_output' : 'java_output';
@@ -20,12 +23,20 @@
 				if($scope.currentTab === 0)  {
 					var options = $scope.xml_options;
 					options.visibility = $scope.xml_opt_visibility;
-					var inflated_xml = android_inflate_xml.inflate_xml(document.getElementById('xml_input').value, options);
-					document.getElementById('results2').value = inflated_xml;
+					var input_val = document.getElementById('xml_input').value;
+					try{
+						var inflated_xml = android_inflate_xml.inflate_xml(input_val, options);
+						document.getElementById('results2').value = inflated_xml.declaration + '\n\n' + inflated_xml.inflated_java;
+					}
+					catch(err){
+						document.getElementById('results2').value = input_val + " could not be parsed.";	
+					}
+					
 				}
 				else{
-					var options = {};
-					document.getElementById('results').value = android_inflate.display_inflate_android_ui(document.getElementById('java_input').value, options);
+					var options = $scope.var_declaration_options;
+					var element_array = android_inflate.element_array_from_declaration(document.getElementById('java_input').value, options);
+					document.getElementById('results').value = android_inflate_xml.inflate_element_list(element_array, options);
 				}
 				$scope.isSelectEnabled = true;
 			};
