aa65ac06e4b3424b5cfb56a3e7bf6182128d084b
Allen Garvey <allengarvey@hotmail.com>
2015-02-08 18:54:14 -0500

    change root directory name

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
 
 
-define("BASE_URL","/android_inflater/");
-define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/android_inflater/");
+define("BASE_URL","/android-inflater/");
+define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/android-inflater/");
